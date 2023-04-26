08de1df31f1c5564f902bf6230fe258a54126314
Jakub Pawlik <jakub.pawlik@vp.pl>
2023-04-26 14:30:24 +0200

    Added task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+galleryItems.forEach(img => {
+  const imageTemplate = `
+  <a class="gallery__item" href="${img.original}">
+  <img class="gallery__image" src="${img.preview}" alt="${img.description}" />
+  </a>
+  `;
+  const ul = document.body.querySelector('.gallery');
+  ul.insertAdjacentHTML('beforeend', imageTemplate);
+});
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+function selectImage() {
+  lightbox.on();
+}
+
+const gallery = document.querySelector('.gallery');
+gallery.addEventListener('click', selectImage);
